f8be3df6427ce168c16531362293af7100669491
itai pendler <212519425@idf.il>
2022-03-04 22:27:31 +0200

    add gridCard story

--- src/components/DrugCard/DrugCard.stories.tsx
+++ src/components/DrugCard/DrugCard.stories.tsx
@@ -21,12 +21,48 @@ export const Playground = () => {
   };
   return <DrugCard drug={drug}></DrugCard>;
 };
+
+export const DrugGrid = () => {
+  const drugs: Drug[] = [
+    {
+      drugNameIndex: 213,
+      amount: 20,
+      unit: 'mg',
+      hasPrescription: true,
+    },
+    {
+      drugNameIndex: number('drugNameIndex2', 421),
+      amount: number('amount2', 10),
+      unit: select('Drug Unit2', ['mg', 'gram'], 'mg'),
+      hasPrescription: boolean('hasPrescription2', true),
+    },
+    {
+      drugNameIndex: number('drugNameIndex3', 21),
+      amount: number('amount3', 5),
+      unit: select('Drug Unit3', ['mg', 'gram'], 'gram'),
+      hasPrescription: boolean('hasPrescription3', false),
+    },
+    {
+      drugNameIndex: number('drugNameIndex4', 321),
+      amount: number('amount4', 50),
+      unit: select('Drug Unit4', ['mg', 'gram'], 'mg'),
+      hasPrescription: boolean('hasPrescription4', true),
+    },
+    {
+      drugNameIndex: number('drugNameIndex5', 500),
+      amount: number('amount5', 120),
+      unit: select('Drug Unit5', ['mg', 'gram'], 'mg'),
+      hasPrescription: boolean('hasPrescription5', true),
+    },
+  ];
+
   return (
-    <DrugCard
-      drugNameIndex={drugNameIndex}
-      amount={amount}
-      unit={unit}
-      hasPrescription={hasPrescription}
-    ></DrugCard>
+    <Grid container spacing={0.5} direction={'row'}>
+      {drugs.map(drug => (
+        <Grid item sm={6} md={4} lg={3}>
+          <DrugCard drug={drug}></DrugCard>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
